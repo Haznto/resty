da1a01192152beccf1573dbb2c3235206f38f5b2
Haznto <haznto@gmail.com>
2023-08-27 07:11:49 +0000

    solved Lab 28

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -2,11 +2,24 @@ import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import App from "../App";
 import '@testing-library/jest-dom/extend-expect';
 
+
+import { server } from '../mocks/server'
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen())
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers())
+
+// Clean up after the tests are finished.
+afterAll(() => server.close())
+
 test('renders Header', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Resty/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+  render(<App />);
+  const linkElement = screen.getByText(/Resty/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 it("test if text Request_Method", () => {
   render(<App />);
@@ -27,8 +40,35 @@ it("test if elements exist", async () => {
 
 });
 describe('Footer Component', () => {
-    it('should render the footer text', () => {
-      render(<App />);
-      expect(screen.getByText('© 2018')).toBeInTheDocument();
+  it('should render the footer text', () => {
+    render(<App />);
+    expect(screen.getByText('© 2018')).toBeInTheDocument();
+  });
+});
+describe('Api Mock Test', () => {
+  test('submits the form and fetches Pokemons', async () => {
+    // const { getByLabelText, getByTestId } = renderWithProviders(<App />);
+    render(<App />);
+    const urlInput = screen.getByLabelText('URL:');
+    const submitButton = screen.getByTestId('submitBtn');
+
+    fireEvent.change(urlInput, { target: { value: 'https://pokeapi.co/api/v2/pokemon' } });
+    fireEvent.click(submitButton);
+
+    // Wait for the API call to complete (assuming it triggers the fetch)
+    await waitFor(async () => {
+      const countDiv = screen.getByTestId('testing-count');
+      const sectionDiv = screen.getByTestId('section-of-data');
+      const countContent = countDiv.textContent;
+      const sectionDivContent = sectionDiv.textContent
+
+      console.log(sectionDivContent)
+      // console.log("Hasan TOmaaaaaaaaaaaaaaaaaaaaaaaaaaaaalieh",countContent)
+      // Check if the expected content is present in the countDiv
+      expect(countContent).toEqual('Count: 1281');
+
+      // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
+      expect(sectionDivContent).toContain('"name": "bulbasaur"');
     });
   });
+});
